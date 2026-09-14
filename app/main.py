8624d69e52be0da63b84c7ac73005c55745d4d25
ErickBortoloti <24sdsee@gmail.com>
2026-09-14 18:23:24 -0300

    teste app para ci/cd

--- app/main.py
+++ app/main.py
@@ -1,7 +1,46 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/status")
+
+@app.get("/", response_class=HTMLResponse)
+
+def home():
+    return """
+        <body style="font-family: Arial; display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0;">
+            <div style="text-align: center;">
+                <h1>API Status Checker</h1>
+                <p>Clique no botão abaixo para verificar o status:</p>
+                <a href="/status">
+                    <button style="padding: 10px 20px; font-size: 16px; cursor: pointer;">
+                        Ver Status
+                    </button>
+                </a>
+            </div>
+        </body>
+    </html>
+            
+
+
+    """
+
+
+
+@app.get("/status", response_class=HTMLResponse)
 def status():
-    return {"status":"ok"}
+    return """
+    <html>
+        <body style="font-family: Arial; display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0;">
+            <div style="text-align: center;">
+                <h1>Status: OK</h1>
+                <p>Tudo funcionando!</p>
+                <a href="/">
+                    <button style="padding: 10px 20px; font-size: 16px; cursor: pointer;">
+                        Voltar
+                    </button>
+                </a>
+            </div>
+        </body>
+    </html>
+    """
