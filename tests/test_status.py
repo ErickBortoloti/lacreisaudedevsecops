34a544705b6e4e5e24ada52733308e90a1bd2862
ErickBortoloti <24sdsee@gmail.com>
2026-09-14 18:27:11 -0300

    ajuste no pytest para response html

--- tests/test_status.py
+++ tests/test_status.py
@@ -4,8 +4,20 @@ from app.main import app
 
 cliente = TestClient(app)
 
-def test_status():
-    response = cliente.get("/status") ## GET no PAth
 
-    assert response.status_code == 200 # Valida resposta em /status 
-    assert response.json() == {"status": "ok"} # Valida o conteúdo do Endpoint
+def test_home():
+    response = cliente.get("/")
+
+    assert response.status_code == 200
+    assert "API Status Checker" in response.text
+    assert "Ver Status" in response.text
+
+
+def test_status():
+    response = cliente.get("/status")
+
+    assert response.status_code == 200
+    assert "Status: OK" in response.text
+    assert "Tudo funcionando!" in response.text
+
+
